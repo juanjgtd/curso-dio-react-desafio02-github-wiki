53c9c008e9018435c71208aba55297dd293c8151
juanjgtd <106997662+juanjgtd@users.noreply.github.com>
2023-04-12 07:57:15 -0300

    Segundo projeto - giti wiki

---
+++ src/components/Button/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { ButtonContainer } from './styles';
+
+function Button({onClick}) {
+  return (
+    <ButtonContainer type='button' onClick={onClick}>
+       Buscar
+    </ButtonContainer>
+  )
+}
+
+export default Button
